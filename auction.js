5358e3e5f65a4d51f241af9c78c75cc2b4649a43
zack-das <izackdas@gmail.com>
2025-11-23 01:44:35 +0300

    Toggle menu function and hamburger icon

--- auction.js
+++ auction.js
@@ -4,3 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menu-icon");
   const closeMenu = document.getElementById("closeMenu");
 
+// Toggle menu function
+  function toggleMobileMenu() {
+    mobileMenu.classList.toggle("active");
+  }
+
+
+  // Open menu when clicking hamburger icon
+  if (menuIcon) {
+    menuIcon.addEventListener("click", function (event) {
+      event.stopPropagation();
+      toggleMobileMenu();
+    });
+  }
